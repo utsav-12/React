1e12f8762ba41a448d6843c7dd35073cd6094a9b
utsav-12 <utsavjain6913@gmail.com>
2021-04-13 22:03:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  let currDate = new Date();
+  currDate = currDate.getHours();
+  let message = '';
+  const cssStyle = {};
+
+  if (currDate >= 1 && currDate < 12) {
+    message = 'Good Morning';
+    cssStyle.color = 'green';
+  }
+  else if (currDate >= 12 && currDate < 19) {
+    message = 'Good Afternoon';
+    cssStyle.color = 'orange';
+  }
+  else {
+    message = 'Good Night';
+    cssStyle.color = 'black';
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>Hello Sir  <span style={cssStyle}> {message} </span></h1>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import "./index.css";
+import App from './App';
 
-let currDate = new Date(2021,5,2,15);
-currDate = currDate.getHours();
-let message = '';
-const cssStyle = {};
 
-if(currDate >=1 && currDate < 12)
-{
-  message = 'Good Morning';
-  cssStyle.color = 'green';
-}
-else if(currDate >=12 && currDate < 19)
-{
-  message = 'Good Afternoon';
-  cssStyle.color = 'orange';
-}
-else{
-  message = 'Good Night';
-  cssStyle.color = 'black';
-}
 
 ReactDom.render(
 
-  <h1> Hello Sir, <span style = {cssStyle}> {message} </span> </h1>,
+  <App />
 
-  document.getElementById('root')
+  ,document.getElementById('root')
 );
